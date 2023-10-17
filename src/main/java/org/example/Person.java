9c2a57ef689e5cea5659fd29a588bc8116bd92b5
Sean <G00420811@atu.ie>
2023-10-17 13:36:07 +0100

    Added two constructors for Person

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -3,13 +3,15 @@ package org.example;
 public class Person {
     public Person() {
         this.firstName = "";
-        this.lastName="";
-        this.age=0;
+        this.lastName = "";
+        this.age = 0;
     }
 
-    public Person(String firstName, String lastName) {
+
+    public Person(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.age = age;
     }
 
     String firstName;
